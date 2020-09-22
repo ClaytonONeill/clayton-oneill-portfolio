ae3c99e0d83f8bee4c13f8b397a0bb96d9fe24cd
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-09-22 16:29:05 -0400

    added some style to the projects pages

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -5,8 +5,15 @@ class Project extends Component {
   render () {
     return (
       <React.Fragment>
-        <button onClick={() => {this.props.handleview('home')}}>Go Back</button>
-        <h1>This is {this.props.projectInfo.title}</h1>
+        <div className='project-contain-all'>
+          <button onClick={() => {this.props.handleview('home')}} id='project-back-button'>Go Back</button>
+          <div className='project-info-contain'>
+            <h1>This is {this.props.projectInfo.title}</h1>
+            <img src={this.props.projectInfo.image} className='shown-project-image'></img>
+            <p>{this.props.projectInfo.description}</p>
+            <a href={this.props.projectInfo.link}>Link to the site!</a>
+          </div>
+        </div>
       </React.Fragment>
     )
   }
